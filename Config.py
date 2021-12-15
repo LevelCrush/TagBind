e96af71ffa0eebf48c99ddb8e8d1387ad788a667
Caelan Dubois <ttMHgame20@gmail.com>
2021-12-15 13:50:09 -0600

    reworked the json file

--- Config.py
+++ Config.py
@@ -13,12 +13,12 @@ class Configuration:
         parser = argparse.ArgumentParser()
         parser.add_argument('input_directory', help='Our initial directory of tags to go through and scan')
         parser.add_argument('output_file', help='The final file destination that we want to render to')
-        parser.add_argument('-recurse', help='Scan directories recursively', action='store_true')
+        parser.add_argument('-recurse', help='Scan directories recursively',default=True)
         parser.add_argument('-count', help='The target amount of clips to aim for per video', default=15)
         parser.add_argument('-platform', help='Helps keep track of a specified platform for tags', default='none')
         parser.add_argument('-shuffle', help='Shuffles the clips instead of sorting them alphabetically',
-                            action='store_false')
-        parser.add_argument('-allow_repeat', help='Allow resuse of clips', action='store_true')
+                            default=False)
+        parser.add_argument('-allow_repeat', help='Allow resuse of clips', default=True)
         parser.add_argument('-no-database-save', help='Do not save in the database')
         parser.add_argument('-add_banners', help='Toggle Banner text')
         parser.add_argument('-width', help='Width of video', default=1280)
@@ -28,8 +28,8 @@ class Configuration:
         parser.add_argument('-acodec', help='FPS of the video', default="aac")
         parser.add_argument('-transition_duration', help='FPS of the video', default=1)
         parser.add_argument('-volume', help='FPS of the video', default=0.35)
-        parser.add_argument('-mute_clips', help='FPS of the video', action='store_false')
-        parser.add_argument('-banners', help='FPS of the video', action='store_true')
+        parser.add_argument('-mute_clips', help='FPS of the video', default=False)
+        parser.add_argument('-banners', help='FPS of the video', default=True)
         command_line_arguments = parser.parse_args()
         # parse command line arguments (old code)
         self.platform = command_line_arguments.platform
@@ -53,6 +53,8 @@ class Configuration:
         self.banners = command_line_arguments.banners
 
 
+
+
         # default paramss
 
 
@@ -60,19 +62,25 @@ class Configuration:
 
 
     def pull_from_configs(self, targeted_platform):
-
+        print("this ran")
         with open('configurations.json') as f:
             data = json.load(f)
 
         print(data)
 
-        for i in data['presets']:
-            if i['name'] == targeted_platform:
-                self.width = int(i['width'])
-                self.height = int(i['height'])
-                self.fps = int(i['fps'])
-                self.vcodec = i['vcodec']
-                self.clip_count = int(i['clip_count'])
+
+        for i in data:
+
+            if i["name"] == targeted_platform:
+                self.width = i["width"]
+                self.height = i["height"]
+                self.fps = i["fps"]
+                self.vcodec = i["vcodec"]
+                self.acodec = i["acodec"]
+
+
+
+
 
 
 
