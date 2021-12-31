5bfc65604274dc83553c8621a708ba16298dca22
Caelan Dubois <ttMHgame20@gmail.com>
2021-12-31 15:35:24 -0600

    removed redundant name variable in configurations.json and fixed a few errors

--- Config.py
+++ Config.py
@@ -24,12 +24,13 @@ class Configuration:
         parser.add_argument('-width', help='Width of video', default=1280)
         parser.add_argument('-height', help='Height of video', default=1440)
         parser.add_argument('-fps', help='FPS of the video', default=60)
-        parser.add_argument('-vcodec', help='FPS of the video', default="libx264")
-        parser.add_argument('-acodec', help='FPS of the video', default="aac")
-        parser.add_argument('-transition_duration', help='FPS of the video', default=1)
-        parser.add_argument('-volume', help='FPS of the video', default=0.35)
-        parser.add_argument('-mute_clips', help='FPS of the video', default=False)
-        parser.add_argument('-banners', help='FPS of the video', default=True)
+        parser.add_argument('-vcodec', help='the video codec', default="libx264")
+        parser.add_argument('-acodec', help='the audio codec', default="aac")
+        parser.add_argument('-transition_duration', help='the transition duration', default=1)
+        parser.add_argument('-volume', help='volume of the video', default=0.35)
+        parser.add_argument('-mute_clips', help='mute clip audio', default=False)
+        parser.add_argument('-banners', help='enable or disable video banners', default=True)
+        parser.add_argument('-font_family', help='FPS of the video', default="Times New Roman")
         command_line_arguments = parser.parse_args()
         # parse command line arguments (old code)
         self.platform = command_line_arguments.platform
@@ -67,22 +68,25 @@ class Configuration:
         with open('configurations.json') as f:
             data = json.load(f)
 
-        print(data)
+        print(data['tiktok']['width'])
 
 
         for i in data:
 
             if i == targeted_platform:
-                self.width = i[1]
-                self.height = i[2]
-                self.fps = i[3]
-                self.vcodec = i[4]
-                self.acodec = i[5]
-                self.font = i[6]
-                self.mute_clips = i[7]
-                self.volume = i[8]
-                self.banners = i[9]
-                self.transition_duration = i[10]    
+
+
+                self.width = int(data[targeted_platform]['width'])
+                self.height = data[targeted_platform]['height']
+                self.fps = data[targeted_platform]['fps']
+                self.vcodec = data[targeted_platform]['vcodec']
+                self.acodec = data[targeted_platform]['acodec']
+                self.clip_count = data[targeted_platform]['clip_count']
+                self.font = data[targeted_platform]['font']
+                self.mute_clips = data[targeted_platform]['mute_clips']
+                self.volume = data[targeted_platform]['music_volume']
+                self.banners = bool(data[targeted_platform]['banners'])
+                self.transition_duration = data[targeted_platform]['transition_duration']
 
 
 
